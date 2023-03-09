dcc9810773b92b3ca3b2df55a83ebde3b00d7a56
kushankur-wisdm <kushankur.das@wisdmlabs.com>
2023-03-09 11:33:37 +0530

    Initialized the Plugin

---
+++ includes/class-calender-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://kushankur.wisdmlabs.net
+ * @since      1.0.0
+ *
+ * @package    Calender
+ * @subpackage Calender/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Calender
+ * @subpackage Calender/includes
+ * @author     Kushankur Das <kushankur.das@wisdmlabs.com>
+ */
+class Calender_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-calender-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://https://kushankur.wisdmlabs.net
+ * @since      1.0.0
+ *
+ * @package    Calender
+ * @subpackage Calender/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Calender
+ * @subpackage Calender/includes
+ * @author     Kushankur Das <kushankur.das@wisdmlabs.com>
+ */
+class Calender_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
